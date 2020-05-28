eb2e13b89ef742b9aeb4901ae113f5d285c33d30
Martin Hübner <martin.hubner@web.de>
2020-05-28 12:37:51 +0200

    adjust config

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 
 var config = {
   // Default language, see i18n.js
-  language: 'en',
+  language: 'de',
   // Show help text for images
   showHelp: true,
   // Files to get data from
   versions: {
   //'SNAPSHOT': '/api/names/SNAPSHOT', // when using sasu backend
-    'SNAPSHOT': 'misc/names-SNAPSHOT.json',
-    '19.07.1': 'misc/names-19.07.1.json',
-    '18.06.7': 'misc/names-18.06.7.json'
+    'gonzo-1.1.0': 'misc/names-SNAPSHOT.json',
+    'hedy-1.0.4': 'misc/names-19.07.1.json'
   },
   // Build custom images
   // See https://github.com/aparcar/asu
